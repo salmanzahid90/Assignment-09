10f5f311e7af8315f241991a4fee5b8af1be0a25
Salman Zahid <122826565+salmanzahid90@users.noreply.github.com>
2023-03-01 14:26:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// 1st problem
+var myVar = 5;
+console.log(myVar);
+
+
+// 2nd problem
+var myString = "Hello World";
+alert(myString);
+
+// 3rd problem
+var num1=5;
+var num2=10;
+var userNum = parseFloat(prompt("Enter your Number"));
+var sum = num1+num2+userNum;
+console.log(sum);
+
+// 4th problem
+var a = false;
+if (a==true){
+    console.log("its true ")
+}else{
+    console.log("its not true")
+}
+
+// 5th problem
+var number = 6;
+var remainder = (number%2);
+if (remainder == 0){
+    console.log(number + "is even");
+}else{console.log(number + "is odd");
+}
+
+// 6th problem
+var a = "Umair";
+var b = "Azmat";
+var c = a+b;
+console.log(c);
+
+// 7th problem
+var a = 5;
+var b = 5;
+var c = a*b;
+console.log(c);
+
+// 8th problem
+var a = prompt("Enter Name");
+var password = "Salman";
+if (password ==a){
+    alert("Access Granted")
+}else{
+    alert("Access Denied");
+}
+  
+// 9th problem
+var n1 = prompt("Enter your 1st number");
+var n2 = prompt("Enter your 2nd number");
+var n3 = prompt("Enter your 3rd number");
+if(n1>=n2 && n1>=n3){
+    alert("n1 is largest number");
+}
+else if(n2>=n2 && n2>=n3){
+    alert("n2 is largest number")
+}else{
+    alert("n3 is largest  number")
+}
+
+// 10th problem
+var a = prompt("Enter String");
+var b = prompt("Enter String");
+if (a==b){
+    alert("The String  are equal")
+}else{
+    alert('The String are not Equal')
+}
+
+
+   
